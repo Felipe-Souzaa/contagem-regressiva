ee5cc499c9a218805238e4e753c25015217aefb8
Felipe Souza <fesouza0705@gmail.com>
2024-01-29 23:38:37 -0300

    começando a codar

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# contagem-regressiva
+# Contagem Regressiva
+
+Nesse projeto irei fazer uma contagem regressiva usando as funções ```time```
---
+++ cont-regressiva.py
@@ -0,0 +1,8 @@
+tempo = float(input("Informe a quantidade do tempo que você quer regressar: "))
+i = 1
+while tempo >= i:
+  print(i)
+  i += 1
+print("tempo acabou")
+
+print(" EU AINDA ESTOU VOLTANDO A CODAR EM PYTHON, ENTÃO DE POUQUINHO A POUQUINHO VOU MELHORANDO ")
